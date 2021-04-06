6bba04cc5cec10e9c6a1d65b0ab351c9e61621c3
mohammadalshuwaiee <shuwaiee1993@gmail.com>
2021-04-06 20:31:31 +0300

    add files

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./component/Home";
 import MoviePage from "./component/MoviePage";
+import Reviews from "./component/Reviews";
+import Watchnow from "./component/Watchnow";
 
 const { Navigator, Screen } = createStackNavigator();
 export default function App() {
@@ -12,6 +14,8 @@ export default function App() {
         <Navigator initialRouteName="Home">
           <Screen name="Home" component={Home} />
           <Screen name="Movie Detail" component={MoviePage} />
+          <Screen name="Reviews" component={Reviews} />
+          <Screen name="Watch Screen" component={Watchnow} />
         </Navigator>
       </NavigationContainer>
     </>
